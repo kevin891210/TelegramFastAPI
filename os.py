0dd446079889a71d63be53ffd639855044236e27
kevin891210 <31150599+kevin891210@users.noreply.github.com>
2025-07-10 12:38:38 +0800

    Update os.py

--- os.py
+++ os.py
@@ -4,9 +4,10 @@ API_ID = int(os.getenv("TG_API_ID", "0"))
 API_HASH = os.getenv("TG_API_HASH", "843b2878d3d02fad55de7043516d3e56")
 TG_SESSION = os.getenv("TG_SESSION")  # leave empty the first run – we’ll print it
 
-N8N_WEBHOOK_URL = os.getenv("N8N_WEBHOOK_URL", "")  # e.g. https://my‑n8n/webhook/telegram‑in
+N8N_WEBHOOK_URL = os.getenv("N8N_WEBHOOK_URL", "https://yhchen.zeabur.app/webhook/f18d6766-9d10-4c79-90a0-41123cb9f7f5")  
+# e.g. https://my‑n8n/webhook/telegram‑in
 
-API_AUTH_TOKEN = os.getenv("API_AUTH_TOKEN", "CHANGE_ME")
+API_AUTH_TOKEN = os.getenv("API_AUTH_TOKEN")
 
 LISTEN_HOST = os.getenv("HOST", "0.0.0.0")
-LISTEN_PORT = int(os.getenv("PORT", "8000"))
+LISTEN_PORT = int(os.getenv("PORT", "8080"))
